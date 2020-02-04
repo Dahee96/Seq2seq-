96fcc73005cb84d79c8cffe823d48d53e8fe9e57
Dahee96 <57829246+Dahee96@users.noreply.github.com>
2020-02-04 18:48:32 +0900

    Update nlp_s2s_cnnaccum.py

--- nlp_s2s_cnnaccum.py
+++ nlp_s2s_cnnaccum.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.optim as optim
 from torchtext.datasets import TranslationDataset, Multi30k
 from torchtext.data import Field, BucketIterator
-#from torchtext.data.metrics import bleu_score
 import torch.nn.functional as F
 import spacy
 import numpy as np
@@ -24,7 +23,6 @@ torch.backends.cudnn.deterministic = True
 #mkdir -p exp/tag
 
 
-
 #load model> de: German, en: English
 spacy_de = spacy.load('de')
 spacy_en = spacy.load('en')
@@ -37,7 +35,8 @@ def tokenize_en(text):
 SRC = Field(tokenize=tokenize_de,
             init_token='<sos>',
             eos_token='<eos>',
-            lower=True)
+            lower=True,
+            include_lengths=True)
 TRG = Field(tokenize=tokenize_en,
             init_token='<sos>',
             eos_token='<eos>',
@@ -62,7 +61,8 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 BATCH_SIZE = 128
 train_iterator, valid_iterator, test_iterator = BucketIterator.splits(
-    (train_data, valid_data, test_data),batch_size = BATCH_SIZE, device=device)
+    (train_data, valid_data, test_data),batch_size = BATCH_SIZE, sort_within_batch = True,
+     sort_key = lambda x : len(x.src), device=device)
 
 
 class Encoder(nn.Module):
@@ -74,22 +74,17 @@ class Encoder(nn.Module):
         self.rnn = nn.LSTM(emb_dim*2, hid_dim, n_layers, dropout=dropout)
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, enc_input):
-        ##packed_outputs, hidden = self.rnn(enc_input)
-        padded_enc_input = nn.utils.rnn.pad_packed_sequence(enc_input)
-        outputs, (hidden, cell) = self.rnn(padded_enc_input)
-        # packed_outputs is a packed sequence containing all hidden states
-        # hidden is now from the final non-padded element in the batch
-        ##outputs, (hidden, cell) = nn.utils.rnn.pad_packed_sequence(packed_outputs)
+    def forward(self, enc_input,src_len):
+        
+        padded_enc_input= nn.utils.rnn.pack_padded_sequence(enc_input, src_len)
+        packed_outputs,(hidden,cell) = self.rnn(padded_enc_input)
         return hidden, cell
 
-    def embedding1(self, src, src_len):
+    def embedding1(self, src):
         # src = [src len, batch size]
-        # src_len = [src len]
         embedded = self.dropout(self.embedding(src))
         # embedded = [src len, batch size, emb dim]
-        packed_embedded = nn.utils.rnn.pack_padded_sequence(embedded, src_len)
-        return packed_embedded
+        return embedded
 
 class Decoder(nn.Module):
     def __init__(self, output_dim, emb_dim, hid_dim, n_layers, dropout):
@@ -105,6 +100,7 @@ class Decoder(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, input, hidden, cell):
+        # input=[batch_size]
         input = input.unsqueeze(0)
         # input = [1, batch size]
         embedded = self.dropout(self.embedding(input))
@@ -119,7 +115,6 @@ class Decoder(nn.Module):
 
     def embedding2(self, trg):
         conv_embedded = self.dropout(self.embedding(trg))
-        #trg 패딩해야하는지 모르겠네...packed_embedded = nn.utils.rnn.pack_padded_sequence(conv_embedded, trg_len)
         return conv_embedded
 
 
@@ -132,21 +127,22 @@ class CNNnet(nn.Module):
         self.scale = torch.sqrt(torch.FloatTensor([0.5])).to(device)
         self.emb2hid = nn.Linear(emb_dim, hid_dim)
         self.hid2emb = nn.Linear(hid_dim, emb_dim)
-        self.fc_out = nn.Linear(output_dim, input_dim)
         self.convs = nn.ModuleList([nn.Conv1d(in_channels=hid_dim,
                                               out_channels=2 * hid_dim,
                                               kernel_size=kernel_size)
                                     for _ in range(conv_layers)])
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, conv_embedded):
-        trg_len =trg.shape[0]
+    def forward(self,src, conv_embedded):
+        src_len = src.shape[0]
+        trg_len =conv_embedded.shape[0]
+        emb_dim = conv_embedded.shape[2]
         # conv_embedded = [trg len, batch size, emb dim]
         conv_input = self.emb2hid(conv_embedded)
         # conv_input = [trg len, batch size, hid dim]
         conv_input = conv_input.permute(1,2,0)
         #conv_input = [batch size, hid dim, trg len]
-        batch_size = conv_input.shape[0] 
+        batch_size = conv_input.shape[0] #다시 정의 왜해?
         hid_dim = conv_input.shape[1]
         for i, conv in enumerate(self.convs):
             conv_input = self.dropout(conv_input)
@@ -161,9 +157,17 @@ class CNNnet(nn.Module):
         conved = self.hid2emb(conved.permute(0, 2, 1))
         # conved = [batch size, trg len, emb dim]
         print('conved.shape', conved.shape)
-        trg_conv = self.fc_out(self.dropout(conved)) # permute해서 바꾼다음에 해야하나 끝 dimension이 바뀌는건가
-        # trg_conv=[batch size, src len, emb dim] #이렇게 예상
-        print('trg_conv.shape',trg_conv.shape)
+        conved=conved.permute(0,2,1)
+        # conved = [batch size, emb dim, trg len]
+        print('permute conved.shape', conved.shape)
+        conved = conved.reshape(batch_size*emb_dim,-1)
+        # conved =[batch size * emb dim, trg len]
+        fc = nn.Linear(trg_len, src_len)
+        trg_conv = fc(self.dropout(conved)) # (batch size * emb dim, trg len) * (trg len, src len)
+        #trg_conv=[batch size * emb dim, src len]
+        trg_conv=trg_conv.view(batch_size,emb_dim,src_len )
+        trg_conv = trg_conv.permute(2,0,1)
+        # trg_conv=[src len, batch size, emb dim]
         return trg_conv
 
 class Seq2Seq(nn.Module):
@@ -179,31 +183,29 @@ class Seq2Seq(nn.Module):
         assert encoder.n_layers == decoder.n_layers, \
             "Encoder and decoder must have equal number of layers!"
 
-    def forward(self, src, trg, teacher_forcing_ratio = 0.5):
+    def forward(self, src, src_len, trg, teacher_forcing_ratio = 0.5):
         # src = [scr len, batch size]
         # trg = [trg len, batch size]
+        # src_len=[batch size] 가 되어야함 int가 아니라
 
         batch_size = trg.shape[1]
-        src_len = src.shape[0]
         trg_len = trg.shape[0]
         trg_vocab_size = self.decoder.output_dim
-       
+        #embedded = self.net(src,trg)
         # tensor to store decoder outputs
         outputs = torch.zeros(trg_len, batch_size, trg_vocab_size).to(self.device)
-
-
-        # last hidden state of the encoder is used as the initial hidden state of the decoder
-        packed_embedded = self.encoder.embedding1(src)
-        # econder랑 conv랑 concat
+        embedded = self.encoder.embedding1(src)
+        print('encoder embedded', embedded.shape)
+        #econder랑 conv랑 concat
         conv_embedded = self.decoder.embedding2(trg)
-        trg_conv = self.cnnnet(conv_embedded)
-        enc_input = torch.cat((packed_embedded, trg_conv), dim=2)
-        hidden, cell = self.encoder(enc_input)
+        trg_conv = self.cnnnet(src,conv_embedded)
+        enc_input = torch.cat((embedded, trg_conv), dim=2)
+        hidden, cell = self.encoder(enc_input,src_len)
 
         # first input to the decoder is the <sos> tokens
         input = trg[0,:]
 
-        for t in range(1,trg_len):
+        for t in range(1, trg_len):
 
             output, hidden, cell = self.decoder(input, hidden, cell)
             outputs[t] = output
@@ -223,6 +225,7 @@ CONV_LAYERS = 2
 N_LAYERS = 2
 ENC_DROPOUT = 0.5
 DEC_DROPPUT = 0.5
+SRC_PAD_IDX = SRC.vocab.stoi[SRC.pad_token]
 TRG_PAD_IDX = TRG.vocab.stoi[TRG.pad_token]
 
 enc = Encoder(INPUT_DIM, ENC_EMB_DIM, HID_DIM, N_LAYERS, ENC_DROPOUT)
@@ -236,6 +239,10 @@ def init_weights(m):
         nn.init.uniform_(param.data, -0.08, 0.08)
 model.apply(init_weights)
 
+#파라메터 수 계산
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+#print('The model has %s trainable parameters' %count_parameters(model)) #13899013
 
 # optimize
 optimizer = optim.Adam(model.parameters())
@@ -248,11 +255,11 @@ def train(model, iterator, optimizer, criterion, clip):
     model.train()
     epoch_loss = 0
     for i, batch in enumerate(iterator):
-        src = batch.src
+        src, src_len = batch.src
         trg = batch.trg
         optimizer.zero_grad()
 
-        output = model(src, trg)
+        output = model(src, src_len, trg)
         # trg = [trg len, batch size]
         # output = [trg len, batch size, output dim]
         output_dim = output.shape[-1]
@@ -275,13 +282,18 @@ def evaluate(model, iterator, criterion):
     epoch_loss = 0
     with torch.no_grad():
         for i, batch in enumerate(iterator):
-            src = batch.src
+            src, src_len = batch.src
             trg = batch.trg
-            output = model(src, trg, 0)
+            output = model(src, src_len, trg, 0)
+            # trg=[trg len, batch size]
+            # output=[trg len, batch size, output dim]
+
             output_dim = output.shape[-1]
             output = output[1:].view(-1, output_dim)
             trg = trg[1:].view(-1)
-          
+            # trg=[(trg len - 1) * batch size]
+            # ouput=[(trg len - 1) * batch size, output dim]
+
             loss = criterion(output, trg)
             epoch_loss += loss.item()
     return epoch_loss / len(iterator)
@@ -294,8 +306,8 @@ def epoch_time(start_time, end_time):
     return elapsed_mins, elapsed_secs
 
 
-N_EPOCHS = 1
-CLIP = 1 
+N_EPOCHS = 10
+CLIP = 1 #??
 best_valid_loss = float('inf')
 
 for epoch in range(N_EPOCHS):
@@ -320,11 +332,19 @@ test_loss = evaluate(model, test_iterator, criterion)
 print('\tTest Loss: %3f | Test PPL: %7.3f' %(test_loss, math.exp(test_loss)))
 
 
-******************************************************************************************
-  File "/home/dh/PycharmProjects/seq2seq/nlp_s2s_cnnaccum.py", line 93, in embedding
-    embedded = self.dropout(self.embedding(src))
-RecursionError: maximum recursion depth exceeded
-    
-    
-    
-    
+from torchtext.data.metrics import bleu_score
+def calculate_bleu(data, src_field, trg_field, model, device, max_len=50):
+    trgs = []
+    pred_trgs = []
+    for datum in data:
+        src = vars(datum)['src']
+        trg = vars(datum)['trg']
+        pred_trg, _ = translate_sentence(src, src_field, trg_field, model, device, max_len)
+        # cut off <eos> token
+        pred_trg = pred_trg[:-1]
+        pred_trgs.append(pred_trg)
+        trgs.append([trg])
+    return bleu_score(pred_trgs, trgs)
+
+bleu_score = calculate_bleu(test_data, SRC, TRG, model, device)
+print('BLEU score = %.2f' %bleu_score*100 )
